4e2aab64350f50663beb9a1e1ec9b3306c725d91
Umar Sayyed <sayyed25507@gmail.com>
2019-04-20 15:40:35 +0530

    new files commited

---
+++ app/src/main/java/com/example/teststopmclient/MainActivity.java
@@ -0,0 +1,98 @@
+package com.example.teststopmclient;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.Button;
+
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
+import ua.naiksoftware.stomp.Stomp;
+import ua.naiksoftware.stomp.StompClient;
+import ua.naiksoftware.stomp.dto.LifecycleEvent;
+import ua.naiksoftware.stomp.dto.StompMessage;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "Cashier";
+    private StompClient mStompClient;
+    private CompositeDisposable compositeDisposable;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+        compositeDisposable = new CompositeDisposable();
+
+        Button btn = findViewById(R.id.button1);
+        btn.setOnClickListener(v -> {
+            setup();
+        });
+    }
+
+    private void setup() {
+
+//        List<StompHeader> headers = new ArrayList<>();
+//        headers.add(new StompHeader("admin", "guest"));
+//        headers.add(new StompHeader("admin", "guest"));
+        mStompClient = Stomp.over(Stomp.ConnectionProvider.OKHTTP, "ws://fuelware-env.mkp8vm63pa.ap-south-1.elasticbeanstalk.com:6614/stomp"+"/websocket");
+
+        mStompClient.withClientHeartbeat(10000).withServerHeartbeat(10000);
+
+        Disposable dispLifecycle = mStompClient.lifecycle()
+                .subscribeOn(Schedulers.io())
+                //.observeOn(AndroidSchedulers.mainThread())
+                .onErrorReturn(error -> new LifecycleEvent(LifecycleEvent.Type.ERROR))
+                .doOnError(throwable -> Log.e(TAG, "Throwable " + throwable.getMessage()))
+                .subscribe(lifecycleEvent -> {
+                    switch (lifecycleEvent.getType()) {
+                        case OPENED:
+                            //MLog.showToast(this, "Stomp connection opened");
+                            Log.e(TAG, "Stomp connection opened");
+                            break;
+                        case ERROR:
+                            Log.e(TAG, "Stomp connection error", lifecycleEvent.getException());
+                            //MLog.showToast(this, "Stomp connection error");
+                            break;
+                        case CLOSED:
+                            mStompClient = null;
+                            Log.e(TAG, "Stomp connection closed");
+                            //MLog.showToast(this, "Stomp connection closed");
+                            break;
+                        case FAILED_SERVER_HEARTBEAT:
+                            Log.e(TAG, "Stomp failed server heartbeat");
+                            //MLog.showToast(this, "Stomp  failed server heartbeat");
+                            break;
+                    }
+                }, throwable -> {
+                    Log.e(TAG, "Error on subscribe topic", throwable);
+                });
+
+        compositeDisposable.add(dispLifecycle);
+
+        // Receive greetings
+        Disposable dispTopic = mStompClient.topic("/topic/6jrllu01nnmiz0xep6ic")
+                .subscribeOn(Schedulers.io())
+                .onErrorReturn(error ->
+                        StompMessage.from(error.getMessage())
+                )
+                //.observeOn(AndroidSchedulers.mainThread())
+                .doOnError(throwable -> Log.e(TAG, "Throwable " + throwable.getMessage()))
+                .subscribe(topicMessage -> {
+                    Log.d(TAG, "Received " + topicMessage.getPayload());
+                    //addItem(mGson.fromJson(topicMessage.getPayload(), EchoModel.class));
+                }, throwable -> {
+                    Log.e(TAG, "Error on subscribe topic", throwable);
+                });
+
+        compositeDisposable.add(dispTopic);
+
+        mStompClient.connect();
+
+    }
+}
+
