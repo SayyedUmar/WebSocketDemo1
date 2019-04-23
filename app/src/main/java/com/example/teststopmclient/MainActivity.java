7506e98d9d38adf505dd4ec514c2a06a9ace7cbe
Umar Sayyed <sayyed25507@gmail.com>
2019-04-24 00:22:38 +0530

    websocket changes finally working code

--- app/src/main/java/com/example/teststopmclient/MainActivity.java
+++ app/src/main/java/com/example/teststopmclient/MainActivity.java
@@ -1,24 +1,22 @@
 package com.example.teststopmclient;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.Button;
 
-import io.reactivex.disposables.CompositeDisposable;
-import io.reactivex.disposables.Disposable;
-import io.reactivex.schedulers.Schedulers;
+import org.java_websocket.WebSocket;
+
 import ua.naiksoftware.stomp.Stomp;
-import ua.naiksoftware.stomp.StompClient;
-import ua.naiksoftware.stomp.dto.LifecycleEvent;
-import ua.naiksoftware.stomp.dto.StompMessage;
+import ua.naiksoftware.stomp.client.StompClient;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "Cashier";
-    private StompClient mStompClient;
-    private CompositeDisposable compositeDisposable;
+//    private StompClient mStompClient;
+//    private CompositeDisposable compositeDisposable;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -26,15 +24,138 @@ public class MainActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_main);
 
-        compositeDisposable = new CompositeDisposable();
+//        compositeDisposable = new CompositeDisposable();
 
         Button btn = findViewById(R.id.button1);
         btn.setOnClickListener(v -> {
-            setup();
+            connectScoket();
         });
     }
 
-    private void setup() {
+    public void connectScoket() {
+        new LongOperation().execute("");
+    }
+
+
+    private class LongOperation extends AsyncTask<String, Void, String> {
+        private StompClient mStompClient;
+        String TAG="LongOperation";
+        @Override
+        protected String doInBackground(String... params) {
+
+            mStompClient = Stomp.over(WebSocket.class, "ws://192.168.0.4:6614/stomp/websocket");
+            //mStompClient = Stomp.over(WebSocket.class, "ws://fuelware-env.mkp8vm63pa.ap-south-1.elasticbeanstalk.com:6614/stomp/websocket");
+            mStompClient.connect();
+
+            mStompClient.topic("/topic/greetingsUmarSayyed").subscribe(topicMessage -> {
+                Log.d(TAG, topicMessage.getPayload());
+            });
+
+            mStompClient.send("/app/hello", "My first STOMP message!").subscribe();
+
+
+            mStompClient.lifecycle().subscribe(lifecycleEvent -> {
+                switch (lifecycleEvent.getType()) {
+
+                    case OPENED:
+                        Log.d(TAG, "Stomp connection opened");
+                        break;
+
+                    case ERROR:
+                        Log.e(TAG, "Error", lifecycleEvent.getException());
+                        break;
+
+                    case CLOSED:
+                        Log.d(TAG, "Stomp connection closed");
+                        break;
+                }
+            });
+            return "Executed";
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+
+        }
+
+    }
+
+
+    /*public void connectScoket() {
+        if (mStompClient != null) {
+            Log.d(TAG, "Already Connected");
+            return;
+        }
+        Log.d(TAG, "Starting StompClient");
+        mStompClient = Stomp.over(WebSocket.class, "ws://fuelware-env.mkp8vm63pa.ap-south-1.elasticbeanstalk.com:6614/stomp/websocket");
+        //mStompClient.withClientHeartbeat(10000).withServerHeartbeat(10000);
+
+        resetSubscriptions();
+        Disposable dispLifecycle = mStompClient.lifecycle()
+                .subscribeOn(Schedulers.io())
+                .doOnError(error -> Log.d(TAG, "The error message is: " + error.getMessage()))
+                .subscribe(lifecycleEvent -> {
+                    switch (lifecycleEvent.getType()) {
+                        case OPENED:
+                            Log.i(TAG, "Stomp connection opened");
+                            break;
+                        case ERROR:
+                            Log.e(TAG, "Stomp connection error", lifecycleEvent.getException());
+                            break;
+                        case CLOSED:
+                            Log.e(TAG, "Stomp connection closed");
+                            destroy();
+                            mStompClient = null;
+                            //connectScoket();
+                            break;
+                        case FAILED_SERVER_HEARTBEAT:
+                            Log.e(TAG, "Stomp failed server heartbeat");
+                            break;
+                    }
+                }, throwable -> {
+                    Log.e(TAG, "Error on subscribe topic", throwable);
+                });
+        compositeDisposable.add(dispLifecycle);
+        subscribeNotifications();
+        mStompClient.connect();
+    }
+    private void subscribeNotifications() {
+
+        Disposable dispTopic = mStompClient.topic("/topic/44236Umar")
+                .subscribeOn(Schedulers.io())
+                .doOnError(error -> Log.d(TAG, "The error message is: " + error.getMessage()))
+                .onErrorReturn(error ->
+                        StompMessage.from(error.getMessage())
+                )
+                .subscribe(topicMessage -> {
+                    Log.d(TAG, "Received " + topicMessage.getPayload());
+
+                }, throwable -> {
+                    Log.e(TAG, "Error on subscribe topic", throwable);
+                });
+        Log.d(TAG, "Subscribe " + "/topic/asdjfksajfk");
+        compositeDisposable.add(dispTopic);
+    }
+
+
+    private void resetSubscriptions() {
+        if (compositeDisposable != null) {
+            compositeDisposable.dispose();
+        }
+        compositeDisposable = new CompositeDisposable();
+    }
+
+    public void destroy() {
+        Log.d(TAG, "Destroying socket stomp client");
+        if (mStompClient != null) {
+            mStompClient.disconnect();
+        }
+        if (compositeDisposable != null) compositeDisposable.dispose();
+        mStompClient = null;
+    }*/
+
+
+    /*private void setup() {
 
 //        List<StompHeader> headers = new ArrayList<>();
 //        headers.add(new StompHeader("admin", "guest"));
@@ -93,6 +214,6 @@ public class MainActivity extends AppCompatActivity {
 
         mStompClient.connect();
 
-    }
+    }*/
 }
 
